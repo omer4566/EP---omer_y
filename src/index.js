144f4119b4ea2420c02ef176226ab4c5ed141310
Yitzhakpro <isako14@gmail.com>
2026-03-24 19:45:10 +0200

    changed

--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ fastify.addHook("onResponse", async (request, reply) => {
       method: request.method,
       url: request.url,
       statusCode: reply.statusCode,
-      responseTime: reply.getResponseTime()
+      responseTime: reply.elapsedTime
     }, `${request.method} ${request.url} - ${reply.statusCode}`);
   }
 });
